ed65f03c5d0a3a6548755d54a2f332e456a0ac8b
YooSungHyun <shyu0522@hanmail.net>
2024-01-05 20:12:59 +0900

    Update: unused file

--- scripts/download_fsdp_example_dataset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Download the files into the "data" folder
-wget -P ./raw_data https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowAll.csv
-wget -P ./raw_data https://public-nlp-datasets.s3.us-west-2.amazonaws.com/wikihowSep.csv
